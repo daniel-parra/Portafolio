6ba7ae09f90a3ddb32388abd35c37a3f743553fd
daniel-parra <danieljanu.parra@gmail.com>
2023-10-19 09:49:30 -0500

    Update prueba.py

--- Scripts/prueba.py
+++ Scripts/prueba.py
@@ -12,7 +12,6 @@ def crear_password(longitud):
     return password
 
 password = crear_password(16)
-print(password) #danger13 - p\r^{kPfd0Nj,U$m
-#https://accounts.palia.com/sign-up?referral=2efb6220-f559-45b8-8485-cfd63205e9fd
+print(password)
 
 
